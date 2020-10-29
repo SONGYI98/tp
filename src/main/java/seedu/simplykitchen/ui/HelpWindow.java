ff584c0f6d20ccb325b1e8bd9990bdc35183b570
SONGYI98 <e0392524@u.nus.edu>
2020-10-29 17:04:25 +0800

    Add command summary in Help window

--- src/main/java/seedu/simplykitchen/ui/HelpWindow.java
+++ src/main/java/seedu/simplykitchen/ui/HelpWindow.java
@@ -16,7 +16,9 @@ import seedu.simplykitchen.commons.core.LogsCenter;
 public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL = "https://ay2021s1-cs2103t-f13-4.github.io/tp/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
+    public static final String USERGUIDE_MESSAGE = "Refer to the user guide for more information: " + USERGUIDE_URL;
+    public static final String HELP_MESSAGE = USERGUIDE_MESSAGE;
+    public static final String COMMAND_SUMMARY = "A summary of the commands is provided below:\n";
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
@@ -27,6 +29,9 @@ public class HelpWindow extends UiPart<Stage> {
     @FXML
     private Label helpMessage;
 
+    @FXML
+    private Label imageDescription;
+
     /**
      * Creates a new HelpWindow.
      *
@@ -35,6 +40,7 @@ public class HelpWindow extends UiPart<Stage> {
     public HelpWindow(Stage root) {
         super(FXML, root);
         helpMessage.setText(HELP_MESSAGE);
+        imageDescription.setText(COMMAND_SUMMARY);
     }
 
     /**
